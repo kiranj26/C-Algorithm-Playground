c05f239a8a00e24de770549177331e62a8b290d9
Kiran Jojare <kijo7257@colorado.edu>
2024-06-22 17:07:46 -0600

    Initial Sorting Code

---
+++ sorting/simple_sort.c
@@ -0,0 +1,41 @@
+/*
+ * File: simple_sort.c
+ * Author: Kiran Jojare
+ * Date: 2022-03-14
+ * 
+ * This program implements the bubble sort algorithm in C. 
+ * It defines a function bubbleSort that takes an array of integers and its length, 
+ * and sorts the array in ascending order using the bubble sort algorithm.
+ * 
+ * The main function initializes an array of integers, calculates its length, 
+ * and calls the bubbleSort function to sort it. 
+ * After sorting, it prints the sorted array to the standard output.
+ */
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    bubbleSort(arr, n);
+    
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
